4f889c9a2c963a2ac6ef501a2711486088a5b836
Anil G <anil.gangadharaiah@neudesic.com>
2018-11-02 20:18:09 +0530

    removed  child

--- src/client/App.js
+++ src/client/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from "react";
-import Child from "./child";
 export default class App extends Component {
   constructor() {
     super();
   }
 
   render() {
-    return (
-      <div>
-        Hi
-        <Child />
-      </div>
-    );
+    return <div>Hi</div>;
   }
 }
